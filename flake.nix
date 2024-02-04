a91445af9bfcb03899d761ac8e19ef9b96000c91
Léana 江 <leana.jiang@icloud.com>
2024-02-04 12:10:04 +0100

    add: nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''
+    The zermo theme, made for Zola
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          zola
+        ];
+      };
+    });
+}
